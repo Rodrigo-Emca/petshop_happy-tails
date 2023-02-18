f075ea45f0d9d089a97dc642d7940b781b6b2a94
Elias Jimenez Diz <eliasj.diz@gmail.com>
2023-02-17 22:45:25 -0500

    funcion modal para arreglar el id del modal

--- assets/scripts/farmacia.js
+++ assets/scripts/farmacia.js
@@ -1,4 +1,4 @@
-import { filtrarPorCategoria, agregarTarjetas, filtrarCoincidencias } from "./module/functions.js"
+import { modal, agregarTarjetas, filtrarCoincidencias } from "./module/functions.js"
 
 
 const $contenedorTarjetas = document.getElementById("contenedorProductos")
@@ -7,34 +7,32 @@ const $keyword = document.getElementById("keyword")
 const myModal = document.getElementById('myModal')
 const myInput = document.getElementById('myInput')
 
-
-let productos;
-let categorias;
-let farmaciaProductos;
+let productosSelec = []
 
 fetch(`https://mindhub-xj03.onrender.com/api/petshop`)
-    .then(data => data.json())
-    .then(response => {
-        console.log(response)
-        productos = response;
-        categorias = [...new Set(productos.map(producto => producto.categoria))]
+.then(data => data.json())
+.then(response => {
+        const productos = response.filter( item => item.categoria === 'farmacia')
+        const farmacia = modal(productos)    
+        // console.log(farmacia);    
+        agregarTarjetas(farmacia,$contenedorTarjetas);
+        
+        $keyword.addEventListener("keyup", () => {
+            $contenedorTarjetas.innerHTML = ``
+            const palabras = ($keyword.value).toLowerCase()
+            const filtrados = filtrarCoincidencias(farmacia,palabras)
+            agregarTarjetas(filtrados,$contenedorTarjetas)
+        })
+        
+        $contenedorTarjetas.addEventListener('click', (e) => {
+            if( e.target.localName === 'button'){
+                productosSelec.push( farmacia.find( item => item._id === e.target.id))
+                console.log(productosSelec);
+            }
+        })
 
-        farmaciaProductos = filtrarPorCategoria(productos, "farmacia")
 
-        agregarTarjetas(farmaciaProductos, $contenedorTarjetas);
-
-    }
-    )
+    })
     .catch(error => console.log(error))
 
 
-//Eventos
-
-$keyword.addEventListener("keyup", () => {
-    $contenedorTarjetas.innerHTML = ``
-
-    const palabras = ($keyword.value).toLowerCase()
-
-    agregarTarjetas(filtrarCoincidencias(farmaciaProductos, palabras), $contenedorTarjetas)
-}
-)
